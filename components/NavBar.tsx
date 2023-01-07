f778f836bc2db5e2d3d0030e608ea617f3dc2179
micahkatz <micahj2110@gmail.com>
2023-01-07 16:31:06 -0600

    updated nav to show/hide

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,11 @@
 import { useMediaQuery } from 'usehooks-ts'
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSsr } from 'usehooks-ts'
 import styles from 'styles/NavBar.module.css'
 import LogoImg from '../assets/logo.png'
+
 type Props = {}
 
 const NavBar = (props: Props) => {
@@ -20,10 +22,60 @@ const NavBar = (props: Props) => {
 
         return matches;
     }
+    function useScroll() {
+        const { isBrowser } = useSsr()
+
+        // storing this to get the scroll direction
+        const [lastScrollTop, setLastScrollTop] = useState(0);
+        // the offset of the document.body
+        const [bodyOffset, setBodyOffset] = useState(
+            isBrowser ? document.body.getBoundingClientRect() : { top: 0, left: 0 }
+        );
+        // the vertical direction
+        const [scrollY, setScrollY] = useState<number>(bodyOffset.top);
+        // the horizontal direction
+        const [scrollX, setScrollX] = useState<number>(bodyOffset.left);
+        // scroll direction would be either up or down
+        const [scrollDirection, setScrollDirection] = useState<'down' | 'up'>();
+
+        const listener = (e: Event) => {
+            isBrowser && setBodyOffset(document.body.getBoundingClientRect());
+            setScrollY(-bodyOffset.top);
+            setScrollX(bodyOffset.left);
+            setScrollDirection(lastScrollTop > -bodyOffset.top ? "down" : "up");
+            setLastScrollTop(-bodyOffset.top);
+        };
+
+        useEffect(() => {
+            window.addEventListener("scroll", listener);
+            return () => {
+                window.removeEventListener("scroll", listener);
+            };
+        });
+
+        return {
+            scrollY,
+            scrollX,
+            scrollDirection
+        };
+    }
+
+    const { scrollX, scrollY, scrollDirection } = useScroll();
     const isNotMobile = useBetterMediaQuery('(min-width: 768px)')
+    const [isVisible, setIsVisible] = React.useState(true)
+    // const [prevScrollPos, setPrevScrollPos] = React.useState(0)
+
+    React.useEffect(() => {
+        setIsVisible(scrollDirection === 'down')
+    }, [scrollDirection])
+
+
+
 
     return (
-        <div className={`bg-primary-background flex flex-row pt-8 flex-1 justify-between sticky top-0 pb-4 z-20`}>
+        <div className={`bg-primary-background flex flex-row pt-8 flex-1 justify-between sticky top-0 pb-4 z-20 transition-all delay-250 duration-500 
+            ${isVisible ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0'}
+        `}>
             {/* <span className='text-primary-text'>MK</span> */}
             <div className='flex flex-1'>
                 <img src='logo.png' className='w-14 object-contain' />
